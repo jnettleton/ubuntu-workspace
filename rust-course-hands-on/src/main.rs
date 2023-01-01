03fd5e712fad179dedc3c95f116252ab7598c188
Jerry Nettleton <jnettleton@starfire60>
2023-01-01 17:59:51 -0600

    fix code

--- rust-course-hands-on/src/main.rs
+++ rust-course-hands-on/src/main.rs
@@ -21,14 +21,29 @@ fn main() -> Result<()> {
 
     let infile = matches.value_of("infile").unwrap_or_default();
     let outfile = matches.value_of("outfile").unwrap_or_default();
-    let silent = matches.value_of("silent").unwrap_or_default();
+    let silent = if matches.is_present("silent") {
+        true
+    } else {
+        !env::var("PV_SILENT").unwrap_or_default().is_empty()
+    };
 
-    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+    let mut reader: Box<dyn Read> = if !infile.is_empty() {
+        Box::new(File::open(infile)?)
+    } else {
+        Box::new(io::stdin())
+    };
+
+    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
+        Box::new(File::create(outfile)?)
+    } else {
+        Box::new(io::stdout())
+    };
+
     let mut total_bytes = 0;
     let mut buffer = [0; CHUNK_SIZE];
 
     loop {
-        let num_read = match io::stdin().read(&mut buffer) {
+        let num_read = match reader.read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
             Err(_) => break,
@@ -37,7 +52,7 @@ fn main() -> Result<()> {
         if !silent {
             eprintln!("\r{}", total_bytes);
         }
-        if let Err(e) = io::stdout().write_all(&buffer[..num_read]).unwrap() {
+        if let Err(e) = writer.write_all(&buffer[..num_read]) {
             if e.kind() == ErrorKind::BrokenPipe {
                 break;
             }
