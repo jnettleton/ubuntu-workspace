1c2a6666d38a7d979212e48917bbaca93b1091bc
Jerry Nettleton <jnettleton@starfire20>
2023-01-01 15:24:01 -0600

    update pipeviewer

--- rust-course-hands-on/src/main.rs
+++ rust-course-hands-on/src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{App, Arg};
+use core::num;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter, ErrorKind, Read, Result, Write};
+
+const CHUNK_SIZE: usize = 16 * 1024;
+
+fn main() -> Result<()> {
+    let matches = App::new("pipeviewer")
+        .arg(Arg::with_name("infile").help("Read from a file instead of stdin."))
+        .arg(
+            Arg::with_name("outfile")
+                .short("o")
+                .long("outfile")
+                .takes_value(true)
+                .help("Write output to a file instead of stdout."),
+        )
+        .arg(Arg::with_name("silent").short("s").long("silent"))
+        .get_matches();
+
+    let infile = matches.value_of("infile").unwrap_or_default();
+    let outfile = matches.value_of("outfile").unwrap_or_default();
+    let silent = matches.value_of("silent").unwrap_or_default();
+
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+    let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
+
+    loop {
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        total_bytes += num_read;
+        if !silent {
+            eprintln!("\r{}", total_bytes);
+        }
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]).unwrap() {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+        }
+    }
+
+    if !silent {
+        eprintln!("\r{}", total_bytes);
+    }
+
+    Ok(())
 }
