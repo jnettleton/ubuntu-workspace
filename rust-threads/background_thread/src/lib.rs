afdb40084abec70433ec3152ca21d08b0fb32b9d
Jerry Nettleton <jnettleton@gmail.com>
2023-02-05 20:40:01 -0600

    save

--- rust-threads/background_thread/src/lib.rs
+++ rust-threads/background_thread/src/lib.rs
@@ -1,12 +1,19 @@
+use std::thread;
 
 pub struct BackgroundThread {
-
 }
 
 impl BackgroundThread {
     pub fn new() -> Self {
         Self {
-            
         }
     }
+
+    pub fn spawn(&mut self) {
+        let handler = thread::spawn(move || {
+            println!("test thread");
+        });
+        handler.join().unwrap();
+    }
+
 }
