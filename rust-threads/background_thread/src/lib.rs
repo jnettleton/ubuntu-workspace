c006b66277d85909abb97de3adeb9cb7f2ae4a91
Jerry Nettleton <jnettleton@starfire20>
2023-02-05 19:43:57 -0600

    save code

---
+++ rust-threads/background_thread/src/lib.rs
@@ -0,0 +1,27 @@
+
+pub struct BackgroundThread {
+
+}
+
+impl BackgroundThread {
+    pub fn new() -> Self {
+        Self {
+            
+        }
+    }
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
--- rust-threads/src/main.rs
+++ rust-threads/src/main.rs
@@ -1,3 +1,10 @@
+extern crate background_thread;
+
+use background_thread::BackgroundThread;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut _bg_thread = BackgroundThread::new();
+    
 }
