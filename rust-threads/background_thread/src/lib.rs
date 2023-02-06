687d68c4d712cff8e8a757c7d3cdda4b4254f8cc
Jerry Nettleton <jnettleton@gmail.com>
2023-02-05 19:52:55 -0600

    save code

--- rust-threads/background_thread/src/lib.rs
+++ rust-threads/background_thread/src/lib.rs
@@ -10,18 +10,3 @@ impl BackgroundThread {
         }
     }
 }
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
