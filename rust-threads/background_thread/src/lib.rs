7667cba56c959c5804e79a1c7132348ace919e3d
Jerry Nettleton <jnettleton@gmail.com>
2023-02-05 21:53:32 -0600

    save code

--- rust-threads/background_thread/src/lib.rs
+++ rust-threads/background_thread/src/lib.rs
@@ -1,19 +1,62 @@
-use std::thread;
+use std::thread::{self, JoinHandle};
+use std::time::Duration;
+use std::{collections::HashMap};
 
 pub struct BackgroundThread {
+    // thread handler
+    handle: Option<JoinHandle<()>>,
+
+    // cancellation token
+
+    // output variable
+    _items: Box<HashMap<i32,i32>>
 }
 
 impl BackgroundThread {
     pub fn new() -> Self {
         Self {
+            handle: None,
+            _items: Box::new(HashMap::new()),
         }
     }
 
-    pub fn spawn(&mut self) {
-        let handler = thread::spawn(move || {
+    pub fn spawn(&mut self) -> Option<&JoinHandle<()>> {
+        let handle = thread::spawn(move || {
             println!("test thread");
         });
-        handler.join().unwrap();
+        self.handle = Some(handle);
+
+        // if handler.is_finished() { return; };
+        // handler.join().unwrap();
+
+        self.handle.as_ref()
     }
 
+    fn _background_thread(&mut self) {
+        let mut count = 5;
+        let interval = Duration::new(1, 0);
+
+        loop {
+            println!("{count}");
+
+            count -= 1;
+            if count == 0 { break };
+
+            thread::sleep(interval);
+        }
+    }
+}
+
+impl Drop for BackgroundThread {
+    fn drop(&mut self) {
+        // match &self.handle {
+        //     Some(h) => {
+        //         if !*h.is_finished() {
+        //             *h.join();
+        //         }
+        //         return
+        //     },
+        //     None => return
+        // }
+    }
 }
