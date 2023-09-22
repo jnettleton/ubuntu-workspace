83b50ecbde7111d9d36d2447e1b29d69f5278286
Jerry Nettleton <jnettleton@gmail.com>
2023-09-22 01:55:48 -0500

    add new kotlin project, save '.idea' folder

---
+++ kotlin-hello/hello.kt
@@ -0,0 +1,9 @@
+// Kotlin Hello World Program
+
+fun main(args: Array<String>) {
+    println("Hello, World!")
+
+    val names = mutableListOf("Abc", "Xyz", "Pqr")
+    names.remove("aaa")
+    names.remove("Abc")
+}
