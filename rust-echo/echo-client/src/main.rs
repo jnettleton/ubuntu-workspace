528af9f008e32f279da419a4c4ae15847af1a183
Jerry Nettleton <jerry.nettleton@smartthings.com>
2023-12-14 23:24:44 -0600

    start echo client

--- rust-echo/echo-client/src/main.rs
+++ rust-echo/echo-client/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use std::error::Error;
+use tokio::net::TcpListener;
+//use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let _tcp_listener = TcpListener::bind("127.0.0.1:8080").await?;
+
     println!("Hello, world!");
+    Ok(())
 }
