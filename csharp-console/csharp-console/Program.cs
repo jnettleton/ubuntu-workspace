ff022c4e44331088afee86194901cdf94f1ac692
Jerry Nettleton <jerry.nettleton@smartthings.com>
2023-12-14 23:57:32 -0600

    add csharp mono project

---
+++ csharp-console/csharp-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_console
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
